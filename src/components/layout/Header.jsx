1f35cdb5867d7e09ff63eff0c7a720583e43af6b
Brent Wilske <wilskeworkflow@gmail.com>
2022-07-09 13:46:20 -0700

    finished temp hamburger nd changed footer icons to svg

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,42 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import HeaderLink from "./HeaderLink";
+import { useState } from "react";
 
 function Header() {
+  const [menuOpen, setmenuOpen] = useState(false);
   return (
     <>
-      <header className="bg-opacity-100 bg-red-100 shadow-xl  font-sans md:flex md:items-center ">
+      <header className="bg-opacity-100 bg-red-100 shadow-xl  flex items-center font-typewriter ">
         <div className=" m-auto flex">
           <Link to="/">
-            <h1 className=" m-auto text-6xl text-center py-8 font-typewriter">
+            <h1 className=" m-auto xl:text-5xl lg:text-4xl text-3xl  text-center py-8">
               Hearts Window Photgraphy
             </h1>
           </Link>
 
-          <div className="space-y-2 md:hidden p-2 ml-auto ">
+          <div
+            className="space-y-2 md:hidden block p-2 ml-auto z-20 "
+            onClick={() => {
+              if (menuOpen) {
+                setmenuOpen(false);
+              } else {
+                setmenuOpen(true);
+              }
+            }}
+          >
             <span className="block w-8 h-0.5 bg-gray-600"></span>
             <span className="block w-8 h-0.5 bg-gray-600"></span>
             <span className="block w-5 h-0.5 bg-gray-600"></span>
           </div>
-        </div>
-        <Link
-          to="/gallery_directory"
-          className="  xl:w-64  w-40 btn-lg btn-primary bg-emerald-800 ml-auto rounded-lg hidden md:flex items-center justify-center "
-        >
-          Work
-        </Link>
 
-        <Link
-          to="/about"
-          className=" xl:w-64  w-40 btn-lg btn-primary bg-emerald-800 text-lg ml-8  rounded-lg hidden md:flex items-center justify-center"
-        >
-          About
-        </Link>
-        <Link
-          to="/contact"
-          className=" shadow-md  xl:w-64 w-40 btn-lg btn-primary bg-emerald-800 text-lg ml-8 mr-8 rounded-lg hidden md:flex items-center justify-center  "
-        >
-          Contact
-        </Link>
+          {menuOpen && (
+            <div className=" py-12 min-w-full bg-emerald-100 min-h-96 flex flex-col md:hidden gap-12 items-center fixed z-10 text-3xl">
+              <HeaderLink
+                data={{ target: "/gallery_directory", title: "Work" }}
+              ></HeaderLink>
+              <HeaderLink
+                data={{ target: "/about", title: "About" }}
+              ></HeaderLink>
+              <HeaderLink
+                data={{ target: "/contact", title: "Contact" }}
+              ></HeaderLink>
+            </div>
+          )}
+        </div>
+        <div className="hidden md:flex lg:gap-16 md:gap-8 xl:mr-16 lg:mr-12 md:mr-8 mr-0">
+          <HeaderLink
+            data={{ target: "/gallery_directory", title: "Work" }}
+          ></HeaderLink>
+          <HeaderLink data={{ target: "/about", title: "About" }}></HeaderLink>
+          <HeaderLink
+            data={{ target: "/contact", title: "Contact" }}
+          ></HeaderLink>
+        </div>
       </header>
     </>
   );
